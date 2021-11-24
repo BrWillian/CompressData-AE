243ba494f0c6cfb7a4a47ee11cdc2a097d65ba31
brwillian <wiliam-m-@hotmail.com>
2021-11-23 22:42:17 -0300

    debug on train

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 import prepare_data
+from model.model import Autoencoder
+from prepare_data import DatasetGenerator
+from view_dataset import view_dataset_representation
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import mse
+import numpy as np
 
-prepare = prepare_data.PrepareData("/media/Shared Disk/Dataset/Imagens-Labor")
-prepare.separe_images_size((1270, 1012), dst1="/media/Shared Disk/Dataset/high/",
-                           dst2="/media/Shared Disk/Dataset/mid/")
+
+dataset_folder = "/media/Shared Disk/Dataset/high/"
+
+#prepare = prepare_data.PrepareData("/media/Shared Disk/Dataset/Imagens-Labor")
+#prepare.separe_images_size((1270, 1012), dst1="/media/Shared Disk/Dataset/high/",
+                           #dst2="/media/Shared Disk/Dataset/mid/")
+
+
+
+#view_dataset_representation(dataset_folder, n_samples=10)
+model = Autoencoder()
+model.build(input_shape=(None, 1280, 1012, 3))
+model.summary()
+
+
+Datagen = DatasetGenerator(dataset_folder)
+Dataset = Datagen.create_dataset()
+print(Datagen.shape)
+model.compile(optimizer=Adam(learning_rate=0.00025), loss="binary_crossentropy",
+              metrics=["loss"])
+
+
+model.fit(Dataset, Dataset, batch_size=32, epochs=30)
